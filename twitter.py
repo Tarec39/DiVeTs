c59bcfcb04cc646433185d558d70e1e3a0380fee
Tarek <itcameranian@gmail.com>
2021-10-23 20:23:50 +0900

    [update] 完成

--- twitter.py
+++ twitter.py
@@ -11,26 +11,35 @@ TWITTER = OAuth1Session(CK, CS, AT, ATS)
 
 URL = "https://api.twitter.com/1.1/statuses/lookup.json"
 
-class nyaa():
+class nyaa:
     def __init__(self, msg):
         self.msg = msg
         self.extractID()
-
+        self.isVideo()
     def extractID(self):
-        digitsList =re.findall(r'\d{10,}', self.msg)
-        if(digitsList):
-            self.digitsStr = "".join(digitsList)
-             
-        else:return
-    pass
+        digits =re.findall(r'\d{10,}', self.msg)
+        if(digits):
+            self.id = "".join(digits)
+            return
 
-def getDirectLink(id):
-    params ={'id' : id}
-    res = TWITTER.get(URL, params=params)
+        else:return
     
-    if res.status_code == 200:
-        directLink = json.loads(res.text)
-        directLink = directLink[0]['extended_entities']['media'][0]['video_info']['variants']
-        return directLink[len(directLink)-2]['url']
-    else:
-        print("Failed: %d" % res.status_code)
+    def isVideo(self):
+        params ={'id' : self.id}
+        res = TWITTER.get(URL, params=params)
+    
+        if res.status_code == 200:
+            tweetObject = json.loads(res.text)
+
+            if "video" in json.dumps(tweetObject):
+                self.tweetObject = tweetObject
+                return
+
+            else:return
+        else:
+            print("Failed: %d" % res.status_code)
+    
+    def extractDirectLink(self):
+        directLink = self.tweetObject[0]['extended_entities']['media'][0]['video_info']['variants']
+        return(directLink[len(directLink)-2]['url'])
+        
