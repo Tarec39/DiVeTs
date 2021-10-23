85fa6c13a8a4d51f02d534c29bdc3dbe661ce844
Tarek <itcameranian@gmail.com>
2021-10-23 17:34:47 +0900

    [update] リンクの判定を実装

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 import discord
 import config
-import getDirectVideoLink
+# import getDirectVideoLink
+import twitter
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("ログインしました。")
+
+@client.event
+async def on_message(msg):
+    if msg.author.bot:
+        return
+
+    if 'https://twitter.com/' in msg.content:
+        await msg.channel.send("Sup")
+        twitter.nyaa(msg.content)
+
+client.run(config.TOKEN)
